b9c444662b73fc231291d60f12eecc0626721a4b
gizmore <gizmore@wechall.net>
2020-11-08 01:42:35 +0100

    6.10-r9358_fixes

--- Module_Memberlist.php
+++ Module_Memberlist.php
@@ -2,6 +2,9 @@
 namespace GDO\Memberlist;
 
 use GDO\Core\GDO_Module;
+use GDO\DB\GDT_Checkbox;
+use GDO\UI\GDT_Bar;
+use GDO\UI\GDT_Link;
 
 /**
  * Show a list of members sorted by user level, title, last_activity
@@ -15,4 +18,18 @@ final class Module_Memberlist extends GDO_Module
     
     public function onLoadLanguage() { return $this->loadLanguage('lang/memberlist'); }
 
+    public function getConfig()
+    {
+        return [
+            GDT_Checkbox::make('hook_left_bar')->initial('1'),
+        ];
+    }
+    
+    public function cfgHookLeftBar() { return $this->getConfigValue('hook_left_bar'); }
+ 
+    public function hookLeftBar(GDT_Bar $nav)
+    {
+        $nav->addField(GDT_Link::make('link_members')->href(href('Memberlist', 'View')));
+    }
+    
 }
