bbbe65420da813cb29419fa42cdd9da6ef1ae8dd
gizmore <gizmore@wechall.net>
2020-11-13 07:39:06 +0100

    6.10-r9375_onInitSidebar

--- Module_Memberlist.php
+++ Module_Memberlist.php
@@ -2,9 +2,8 @@
 namespace GDO\Memberlist;
 
 use GDO\Core\GDO_Module;
-use GDO\DB\GDT_Checkbox;
-use GDO\UI\GDT_Bar;
 use GDO\UI\GDT_Link;
+use GDO\UI\GDT_Page;
 
 /**
  * Show a list of members sorted by user level, title, last_activity
@@ -18,18 +17,22 @@ final class Module_Memberlist extends GDO_Module
     
     public function onLoadLanguage() { return $this->loadLanguage('lang/memberlist'); }
 
-    public function getConfig()
-    {
-        return [
-            GDT_Checkbox::make('hook_left_bar')->initial('1'),
-        ];
-    }
+//     public function getConfig()
+//     {
+//         return [
+//             GDT_Checkbox::make('hook_left_bar')->initial('1'),
+//         ];
+//     }
     
-    public function cfgHookLeftBar() { return $this->getConfigValue('hook_left_bar'); }
+//     public function cfgHookLeftBar() { return $this->getConfigValue('hook_left_bar'); }
  
-    public function hookLeftBar(GDT_Bar $nav)
+    public function onInitSidebar()
     {
-        $nav->addField(GDT_Link::make('link_members')->href(href('Memberlist', 'View')));
+//         if ($this->cfgHookLeftBar())
+        {
+            $nav = GDT_Page::$INSTANCE->leftNav;
+            $nav->addField(GDT_Link::make('link_members')->href(href('Memberlist', 'View')));
+        }
     }
     
 }
