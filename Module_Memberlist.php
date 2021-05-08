ab235b6abc90598f565d73fe8e19a858baa8904f
gizmore <gizmore@wechall.net>
2021-05-08 05:13:02 +0200

    6.10.2-r1083

--- Module_Memberlist.php
+++ Module_Memberlist.php
@@ -2,14 +2,17 @@
 namespace GDO\Memberlist;
 
 use GDO\Core\GDO_Module;
+use GDO\DB\GDT_Checkbox;
 use GDO\UI\GDT_Link;
 use GDO\UI\GDT_Page;
+use GDO\DB\Cache;
+use GDO\User\GDO_User;
 
 /**
  * Show a list of members sorted by user level, title, last_activity
  * @author gizmore
- * @version 6.10
- * @since 6.10
+ * @version 6.10.2
+ * @since 6.10.0
  */
 final class Module_Memberlist extends GDO_Module
 {
@@ -17,22 +20,32 @@ final class Module_Memberlist extends GDO_Module
     
     public function onLoadLanguage() { return $this->loadLanguage('lang/memberlist'); }
 
-//     public function getConfig()
-//     {
-//         return [
-//             GDT_Checkbox::make('hook_left_bar')->initial('1'),
-//         ];
-//     }
+    public function getConfig()
+    {
+        return [
+            GDT_Checkbox::make('hook_left_bar')->initial('1'),
+        ];
+    }
     
-//     public function cfgHookLeftBar() { return $this->getConfigValue('hook_left_bar'); }
+    public function cfgHookLeftBar() { return $this->getConfigValue('hook_left_bar'); }
  
     public function onInitSidebar()
     {
-//         if ($this->cfgHookLeftBar())
+        if ($this->cfgHookLeftBar())
         {
             $nav = GDT_Page::$INSTANCE->leftNav;
-            $nav->addField(GDT_Link::make('link_members')->href(href('Memberlist', 'View')));
+            $nav->addField(GDT_Link::make('list_memberlist_view', [$this->getMembercount()])->href(href('Memberlist', 'View')));
         }
+    }
+    
+    public function getMembercount()
+    {
+        if (false === ($count = Cache::get('memberlist_membercount')))
+        {
+            $count = GDO_User::table()->countWhere('user_type="member"');
+            Cache::set('memberlist_membercount', $count);
+        }
+        return $count;
     }
     
 }
